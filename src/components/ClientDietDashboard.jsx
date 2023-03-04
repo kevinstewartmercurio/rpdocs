f9f6600bbcb809e0c72e70adfcbdede0abcf3975
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2023-03-04 01:04:19 -0500

    updated navigation

---
+++ src/components/ClientDietDashboard.jsx
@@ -0,0 +1,14 @@
+import { Heading } from "@/components/Heading"
+
+export function ClientDietDashboard() {
+    return (
+        <>
+            <Heading level={2} id="client-diet-dashboard">
+                Client Diet Dashboard
+            </Heading>
+            <p>
+                This is information about the client diet dashboard.
+            </p>
+        </>
+    )
+}
---
+++ src/components/DietDashboard.jsx
@@ -0,0 +1,14 @@
+import { Heading } from "@/components/Heading"
+
+export function DietDashboard() {
+    return (
+        <>
+            <Heading level={2} id="diet-dashboard">
+                Diet Dashboard
+            </Heading>
+            <p>
+                This is information about the diet dashboard.
+            </p>
+        </>
+    )
+}
