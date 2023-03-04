fffde1f0205f006fd58170e18bda95ab459a6b77
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2023-03-04 10:55:55 -0500

    updated client dashboard page

--- src/components/ClientDietDashboard.jsx
+++ src/components/ClientDietDashboard.jsx
@@ -6,9 +6,6 @@ export function ClientDietDashboard() {
             <Heading level={2} id="client-diet-dashboard">
                 Client Diet Dashboard
             </Heading>
-            <p>
-                This is information about the client diet dashboard.
-            </p>
         </>
     )
 }
--- src/components/DietDashboard.jsx
+++ src/components/DietDashboard.jsx
@@ -6,9 +6,6 @@ export function DietDashboard() {
             <Heading level={2} id="diet-dashboard">
                 Diet Dashboard
             </Heading>
-            {/* <p>
-                This is information about the diet dashboard.
-            </p> */}
         </>
     )
 }
