b13630b7446b083795dd48cf97d1c8f2806dbd98
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2023-03-04 02:15:54 -0500

    updated coach dashboard page

--- src/components/DietDashboard.jsx
+++ src/components/DietDashboard.jsx
@@ -6,9 +6,9 @@ export function DietDashboard() {
             <Heading level={2} id="diet-dashboard">
                 Diet Dashboard
             </Heading>
-            <p>
+            {/* <p>
                 This is information about the diet dashboard.
-            </p>
+            </p> */}
         </>
     )
 }
